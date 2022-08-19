c98b3bd0aa9c82ecfa4f1f8f86110e174c936599
Akashay Anand <akashayanand1@gmail.com>
2022-08-19 10:22:09 +0530

    starting Java Script

---
+++ learningJavaScript.js
@@ -0,0 +1,20 @@
+<script>
+
+
+    
+</script>
+
+
+/*
+
+// VARIABLE
+* variable deccleration is independent of datatype (like python)
+* datatype can be changed 
+var x = true;
+x = 2.5;
+x = 5;
+
+
+
+
+*/
